c0597d12fd1fca63c617a0186c4d7fd562556e53
ohowells <ohowells1@gmail.com>
2015-06-17 13:22:29 +0100

    Added FileIO class

---
+++ Delimiter-separated-values/FileIO.h
@@ -0,0 +1,39 @@
+#ifndef _FILE_IO_
+#define _FILE_IO_
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace FileIO
+{
+	template<typename T = int>
+	using Vector_1D = std::vector<T>;
+
+	template<typename T = int>
+	using Vector_2D = std::vector<std::vector<T>>;
+
+	class FileIO
+	{
+	public:
+		void ReadFileIn(std::ifstream& infile, Vector_2D<>& data)
+		{
+			while(std::getline(infile, t_data))
+			{
+				std::istringstream t_stream(t_data);
+				Vector_1D<> t_store;
+
+				// Takes in the string stream and removes the commas,
+				// the pushs each value to a vector
+				while(std::getline(t_stream, ss_data, ','))
+					t_store.push_back(atoi(ss_data.c_str()));
+
+				// Contents of the 1D vector then pushed to a 2D vector
+				data.push_back(t_store);
+			}
+	private:
+		std::string t_data, ss_data;
+	};
+}
+#endif
