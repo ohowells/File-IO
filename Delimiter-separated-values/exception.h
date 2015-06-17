5202ef425421e298982ff09f6ca29d4b01002a6d
ohowells <ohowells1@gmail.com>
2015-06-17 13:30:09 +0100

    Added exception class

---
+++ Delimiter-separated-values/exception.h
@@ -0,0 +1,16 @@
+#ifndef _MY_EXCEPTION_
+#define _MY_EXCEPTION_
+
+#include <stdexcept>
+#include <string>
+
+class MyException
+	: public std::runtime_error
+{
+public:
+	NoFile(const std::string& strError) 
+		: std::runtime_error(strError) 
+	{
+	};
+};
+#endif
