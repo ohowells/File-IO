1647d9c75741359fd3f9159c483f4d315230d979
ohowells <ohowells1@gmail.com>
2015-06-17 15:55:53 +0100

    Updated main function

--- Delimiter-separated-values/main.cpp
+++ Delimiter-separated-values/main.cpp
@@ -2,24 +2,24 @@
 #include "FileIO.h"
 #include "exception.h"
 
-void PrintResults(const FileIO::Vector_2D<>& results)
+void PrintResults(const fio::Vector_2D<>& results)
 {
-	for (const FileIO::Vector_1D<>& v : results)
+	for (const fio::Vector_1D<>& v : results)
 		for (int i : v)
 			std::cout << i << " \n";
 }
 
 int main(void)
 {
-	FileIO::FileIO file_manager;
-	FileIO::Vector_2D<> data_set;
+	fio::FileIO file_manager;
+	fio::Vector_2D<> data_set;
 
 	try
 	{
 		std::ifstream infile("data_base.txt");
-	
+
 		if (!infile)
-			throw NoFile("\nCould not open file.\n\n");
+			throw MyException("\nCould not open file.\n\n");
 
 		file_manager.ReadFileIn(infile, data_set);
 	}
@@ -29,7 +29,7 @@ int main(void)
 		return 1;
 	}
 
-	std::cout << "Reading file that contains Delimiter-separated values.";
+	std::cout << "Reading file that contains Delimiter-separated values.\n";
 	PrintResults(data_set);
 	return 0;
 }
